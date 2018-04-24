68489506100e9dada23b16842b8ba8c67da03be1
l.mirkowski <mirkowskilukasz@gmail.com>
2018-04-24 16:34:09 +0200

    Dodana klasa HibernateUtil i plik hibernate.cfg.xml, bez mapowania encji (brak jeszcze klas modelu)

---
+++ src/main/java/pl/mirkowski/lukasz/util/HibernateUtil.java
@@ -0,0 +1,33 @@
+package pl.mirkowski.lukasz.util;
+
+import java.util.Objects;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+	
+	private static SessionFactory sessionFactory;
+	
+	private static SessionFactory buildSessionFactory() {
+		
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		
+		ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		
+		SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
+		return sessionFactory;
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		
+		if(Objects.isNull(sessionFactory)) {
+			sessionFactory = buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+
+}
